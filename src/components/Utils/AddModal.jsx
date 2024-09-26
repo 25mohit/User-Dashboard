d6bf9d54253b4830be78705b22ac95db379a74db
25mohit <mohit724agarwal@gmail.com>
2024-09-26 21:35:57 +0530

    Code Refractored

--- src/components/Utils/AddModal.jsx
+++ src/components/Utils/AddModal.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Button, Modal } from 'react-bootstrap'
 
 const AddModal = ({ isEmpty, setIsEmpty, showModal, setShowModal, newUser, setNewUser, handleAdd }) => {
--- src/components/Utils/Loading.jsx
+++ src/components/Utils/Loading.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const Loading = () => {
   return (
     <div className="loading-screen">
-        <div className="loader">
-            <div className="loader-inner"></div>
-        </div>
+      <div className="loader">
+        <div className="loader-inner"></div>
+      </div>
     </div>
   )
 }
