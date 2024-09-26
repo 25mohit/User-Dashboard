56d35bcaa524eb131f904fe2dbe86b9da68d59a6
25mohit <mohit724agarwal@gmail.com>
2024-09-26 20:47:43 +0530

    Loading added while Deleting a record

--- src/components/TableContainer.jsx
+++ src/components/TableContainer.jsx
@@ -3,23 +3,35 @@ import { Button } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
 import { useDispatch } from 'react-redux';
 import { deleteUser, updateUser } from '../redux/userSlice';
+import Loading from './Utils/Loading';
 
 const TableContainer = ({ data }) => {
     const dispatch = useDispatch();
     const [isEditing, setIsEditing] = useState(null);
     const [editedUser, setEditedUser] = useState({});
-
-    const handleDelete = (userId) => {
-    dispatch(deleteUser(userId));
+    const [isDeleting, setIsDeleting] = useState(null);
+  
+    const handleDelete = async (userId) => {
+      setIsDeleting(true);
+      try {
+        await dispatch(deleteUser(userId));
+        setIsDeleting(false);
+      } catch (error) {
+        console.error('Error deleting user:', error);
+        setIsDeleting(false);
+      }
     };
+
+    console.log("isDeleting", isDeleting);
+    
 
     const handleEdit = (user) => {
-    setIsEditing(user.id);
-    setEditedUser({ ...user });
+        setIsEditing(user.id);
+        setEditedUser({ ...user });
     };
     const handleSave = () => {
-    dispatch(updateUser(editedUser));
-    setIsEditing(null);
+        dispatch(updateUser(editedUser));
+        setIsEditing(null);
     };
   return (
     <Table striped bordered hover>
@@ -34,21 +46,37 @@ const TableContainer = ({ data }) => {
         </tr>
       </thead>
       <tbody>
-        {
-            data?.map((dt) => 
-                <tr key={dt?.id} className='row-n'>
-                    <td>{dt?.id}</td>
-                    <td>{dt?.name}</td>
-                    <td>{dt?.email}</td>
-                    <td>{dt?.phone}</td>
-                    <td>{dt?.address?.city} ({dt?.address?.zipcode})</td>
-                    <td className='controls'>
-                        <Button size="sm" variant="primary">Edit</Button>
-                        <Button size="sm" variant="danger">Delete</Button>
-                    </td>
-                </tr>)
-        }
+      {data?.map((dt) => (
+          <tr key={dt?.id} className='row-n'>
+            <td>{dt?.id}</td>
+            <td>{isEditing === dt.id ? (
+              <input type="text" value={editedUser.name} onChange={(e) => setEditedUser({ ...editedUser, name: e.target.value })} />
+            ) : (
+              dt?.name
+            )}</td>
+            <td>{isEditing === dt.id ? (
+              <input type="text" value={editedUser.email} onChange={(e) => setEditedUser({ ...editedUser, email: e.target.value })} />
+            ) : (
+              dt?.email
+            )}</td>
+            <td>{isEditing === dt.id ? (
+              <input type="text" value={editedUser.phone} onChange={(e) => setEditedUser({ ...editedUser, phone: e.target.value })} />
+            ) : (
+              dt?.phone
+            )}</td>
+            <td>{dt?.address?.city} ({dt?.address?.zipcode})</td>
+            <td className='controls'>
+              {isEditing === dt.id ? (
+                <Button size="sm" variant="primary" onClick={handleSave}>Save</Button>
+              ) : (
+                <Button size="sm" variant="primary" onClick={() => handleEdit(dt)}>Edit</Button>
+              )}
+              <Button size="sm" variant="danger" onClick={() => handleDelete(dt.id)}>Delete</Button>
+            </td>
+          </tr>
+        ))}
       </tbody>
+      { isDeleting && <Loading /> }
     </Table>
   )
 }
---
+++ src/components/Utils/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="loading-screen">
+        <div className="loader">
+            <div className="loader-inner"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Loading
